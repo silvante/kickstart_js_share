894654595ee766e618bed6ab07c23d8d9a319872
silvante <khamidov.ko@gmail.com>
2025-06-29 01:58:18 +0500

    main frameworks

---
+++ kickstart_client/app/(global_components)/Frameworks.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Frameworks() {
+  return (
+    <div className="w-full flex gap-10 items-center justify-between">
+      <div className="flex-1 bg-black/50 rounded-2xl p-8 flex gap-10 items-center">
+        <div>
+          <Image
+            src={"/images/nest.svg"}
+            alt="nest js logo"
+            width={180}
+            height={180}
+          />
+        </div>
+        <div className="space-y-2">
+          <h3 className="font-semibold text-lg">Nest JS</h3>
+          <p>
+            Pre configured with mailers, JWT, Prisma with PostgreSQL and
+            Background Jobs with BullMQ
+          </p>
+          <Link
+            href={"https://nestjs.com/"}
+            target="_blanck"
+            className="text-violet-500"
+          >
+            visit website
+          </Link>
+        </div>
+      </div>
+      <div className="flex-1 bg-black/50 rounded-2xl p-8 flex gap-10 items-center">
+        <div>
+          <Image
+            src={"/images/next.svg"}
+            alt="nest js logo"
+            width={180}
+            height={180}
+          />
+        </div>
+        <div className="space-y-2">
+          <h3 className="font-semibold text-lg">Next JS</h3>
+          <p>
+            Clean API via axios architecture, pre configured Redux toolkit.
+            Root, Authentication and Panel pages.
+          </p>
+          <Link
+            href={"https://nextjs.org/"}
+            target="_blanck"
+            className="text-violet-500"
+          >
+            visit website
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- kickstart_client/app/page.tsx
+++ kickstart_client/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Hero from "./(global_components)/Hero";
+import Frameworks from "./(global_components)/Frameworks";
 
 export default function Home() {
   return (
-    <div>
+    <div className="space-y-10">
       <Hero />
+      <p className="text-2xl font-semibold text-center">Main frameworks</p>
+      <Frameworks />
     </div>
   );
 }
