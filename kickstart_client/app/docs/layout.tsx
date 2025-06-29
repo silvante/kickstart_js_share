a0881bec5a28bbead8972c5814129d48be61cc47
silvante <khamidov.ko@gmail.com>
2025-06-30 00:30:28 +0500

    code vise

--- kickstart_client/app/docs/layout.tsx
+++ kickstart_client/app/docs/layout.tsx
@@ -15,6 +15,65 @@ export const metadata: Metadata = {
   title: "Kickstart JS uz | Docs",
   description:
     "Kickstart JS Uzbekistan, Open source Startup Template generator with Authentication documentation",
+  keywords: [
+    "Kickstart JS",
+    "kickstart js",
+    "kickstart js uz",
+    "Kickstart JS Uz",
+    "Kickstart Uz",
+    "kickstart uz",
+    "Authentication generator uzbekistan js",
+    "Structured template",
+    "Mardonbek Khamidov",
+    "mardonbek khamidov",
+    "Mardonbek Xamidov",
+    "mardonbek xamidov",
+    "Mardonbek Hamidov",
+    "mardonbek hamidov",
+    "sayt",
+    "Template",
+    "kickstartjs",
+    "kickstartjs uz",
+    "kickstart-js-uz",
+  ],
+  alternates: {
+    canonical: "https://kickstartjs.uz",
+  },
+  openGraph: {
+    title: "Kickstart JS | Uzbekistan",
+    description:
+      "Kickstart JS Uzbekistan Open source Startup Template generator with Authentication",
+    url: "https://xamidov.com",
+    siteName: "Kickstart JS | Uzbekistan",
+    // continue
+    images: [
+      {
+        url: "https://github.com/silvante/portfolio/blob/main/app/assets/og_image.png?raw=true",
+        width: 280,
+        height: 280,
+        type: "image/png",
+        alt: "Mardonbek Khamidov Portfolio Preview",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mardonbek Khamidov | Portfolio",
+    description:
+      "Welcome to the official portfolio of Mardonbek Khamidov — a Full-Stack Software Engineer specializing in high-performance web applications using Ruby on Rails, Next.js, and NestJS. Discover my work, skills, and digital journey.",
+    images: [
+      "https://github.com/silvante/portfolio/blob/main/app/assets/og_image.png?raw=true",
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
   icons: {
     icon: "/icons/icon.svg",
   },
