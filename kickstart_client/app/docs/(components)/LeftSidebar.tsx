50446917d88d6caf19751a117583d0b0183b7608
silvante <khamidov.ko@gmail.com>
2025-06-29 23:18:31 +0500

    and now I should do responsive :(

--- kickstart_client/app/docs/(components)/LeftSidebar.tsx
+++ kickstart_client/app/docs/(components)/LeftSidebar.tsx
@@ -1,5 +1,6 @@
 import NavLink from "@/app/(global_components)/NavLink";
 import AsideLink from "./AsideLink";
+import Link from "next/link";
 
 export default function LeftSidebar() {
   return (
@@ -7,6 +8,10 @@ export default function LeftSidebar() {
       <AsideLink href="/docs">Intoduction</AsideLink>
       <AsideLink href="/docs/installation">Installation</AsideLink>
       <AsideLink href="/docs/usage">Usage</AsideLink>
+      <div className="mt-5 p-4 bg-black/50 rounded-xl flex flex-col gap-3">
+        <Link href={"https://nestjs.com/"} target="_blanck">Nest js website ⚡️</Link>
+        <Link href={"https://nextjs.org/"} target="_blanck">Next js website 💫</Link>
+      </div>
     </aside>
   );
 }
--- kickstart_client/app/docs/(components)/RightSidebar.tsx
+++ kickstart_client/app/docs/(components)/RightSidebar.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function RightSidebar() {
   return (
-    <aside className="w-72 sticky top-26">
-      right
+    <aside className="w-72 sticky top-26 space-y-5">
+      <h4 className="font-semibold">Author</h4>
+      <Link href={"https://xamidov.uz"} target="_blanck">
+        <div className="p-3 rounded-xl bg-black/50 space-y-3">
+          <Image
+            src={"/images/image.png"}
+            alt="Kickstart js author's portfolio"
+            width={400}
+            height={200}
+            className="w-full"
+          />
+          <p>Mardonbek Khamidov | Portfolio</p>
+        </div>
+      </Link>
     </aside>
   );
 }
